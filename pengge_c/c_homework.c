ac2e180860912744755d3abdf7056d319a7a9c7d
xiaomengxiang1 <1797727234@qq.com>
2025-03-11 23:29:10 +0800

    linux

--- pengge_c/c_homework.c
+++ pengge_c/c_homework.c
@@ -1023,17 +1023,18 @@
 // }
 
 //乘法表
-#include <stdio.h>
-int main() {
-    int i = 0;
-    int j = 0;
-    int n = 0;
-    scanf("%d", &n);
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= i; j++) {
-            printf("%d * %d = %d  ", i, j, i*j);
-        }
-        printf("\n");
-    }
-    return 0;
-}
+// #include <stdio.h>
+// int main() {
+//     int i = 0;
+//     int j = 0;
+//     int n = 0;
+//     scanf("%d", &n);
+//     for (i = 1; i <= n; i++) {
+//         for (j = 1; j <= i; j++) {
+//             printf("%d * %d = %d  ", i, j, i*j);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
