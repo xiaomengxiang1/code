64a4e0f5501b8c6d611ea82049bd5a6dd715ef97
xiaomengxiang1 <1797727234@qq.com>
2025-06-02 23:54:36 +0800

    BinaryTree

---
+++ data_struct/BinaryTree.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char BTDataType;
+typedef struct BinaryTreeNode {
+    BTDataType data;
+    struct BinaryTreeNode* left;
+    struct BinaryTreeNode* right;
+}BTNode;
+
+//创建节点
+BTNode* CreatBTNode(char x) {
+    BTNode* node = (BTNode*)malloc(sizeof(BTNode));
+    node->data = x;
+    node->left = NULL;
+    node->right = NULL;
+
+    return node;
+}
+
+//树的大小
+int TreeSize(BTNode* root) {
+    if (root == NULL)
+        return 0;
+    else 
+        return 1 + TreeSize(root->left) + TreeSize(root->right);
+}
+
+//叶子数量
+int TreeLeafSize(BTNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    if (root->left == NULL && root->right == NULL) {
+        return 1;
+    }
+    return TreeLeafSize(root->left) + TreeLeafSize(root->right);
+}
+
+//树的深度
+int TreeDepth(BTNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    int left_tree_depth = TreeDepth(root->left);
+    int right_tree_depth = TreeDepth(root->right);
+    return (left_tree_depth > right_tree_depth ? left_tree_depth : right_tree_depth) + 1;
+}
+
+void PrevOrder(BTNode* root) {
+    if (root == NULL) {
+        printf("NULL ");
+        return;
+    }
+    printf("%c ", root->data);
+    PrevOrder(root->left);
+    PrevOrder(root->right);
+}
+
+void InOrder(BTNode* root) {
+    if (root == NULL) {
+        printf("NULL ");
+        return;
+    }
+    InOrder(root->left);
+    printf("%c ", root->data);
+    InOrder(root->right);
+}
+
+void PostOrder(BTNode* root) {
+    if (root == NULL) {
+        printf("NULL ");
+        return;
+    }
+    PostOrder(root->left);
+    PostOrder(root->right);
+    printf("%c ", root->data);
+}
+
+int main() {
+    BTNode* A = CreatBTNode('A');
+    BTNode* B = CreatBTNode('B');
+    BTNode* C = CreatBTNode('C');
+    BTNode* D = CreatBTNode('D');
+    BTNode* E = CreatBTNode('E');
+    A->left = B;
+    A->right = C;
+    B->left = D;
+    B->right = E;
+
+    // 先根遍历
+    PrevOrder(A);
+    printf("\n");
+    // 中根遍历
+    InOrder(A);
+    printf("\n");
+    // 后根遍历
+    PostOrder(A);
+    printf("\n");
+
+    printf("TreeSize:%d\n", TreeSize(A));
+    printf("TreeLeafSize:%d\n", TreeLeafSize(A));
+    printf("TreeDepth:%d\n", TreeDepth(A));
+    getchar();
+    return 0;
+}
--- data_struct/test_3.c
+++ data_struct/test_3.c
@@ -86,4 +86,137 @@
 // 排列升序：使用大堆
 // 排列降序：使用小堆
 
+// TOPK问题
+
+// 在 N 个数中找出最大的或最小的前 K 个，用什么方法？
+// 正确答案：建小堆！
+// 原理：
+// 建一个 K 个元素的小堆（最小堆）。
+// 堆顶是当前 K 个数中最小的那个。
+// 遍历后面数据时：
+// 如果比堆顶大，就把堆顶踢出去，换新数据进来；
+// 否则不变。
+// 这样：
+// 最终堆里就保留了 最大的 K 个数，而且堆顶是 K 个里最小的，方便下一轮判断！
+
+// 最小k个数
+// https://leetcode.cn/problems/smallest-k-lcci/description/
+// void Swap(int* e1, int* e2) {
+//     int tmp = *e1;
+//     *e1 = *e2;
+//     *e2 = tmp;
+// }
+
+// void AdjustDown(int* a, int n, int root) {
+//     int parent = root;
+//     int child = parent * 2 + 1; // 左孩子
+//     while (child < n) {
+//         //找大的孩子
+//         if (child + 1 < n && a[child + 1] > a[child]) {
+//             ++child;
+//         }
+//         if (a[child] > a[parent]) {
+//             Swap(&a[child], &a[parent]);
+//             parent = child;
+//             child = parent * 2 + 1;
+//         }
+//         else {
+//             break;
+//         }
+//     }
+// }
+
+// int* smallestK(int* arr, int arrSize, int k, int* returnSize) {
+//     if (k == 0 || arrSize == 0) {
+//         *returnSize = 0;
+//         return NULL;
+//     }
+//     //建堆(以前k个为大堆)
+//     int* a = (int*)malloc(sizeof(int) * k);
+//     int i = 0;
+//     for (i = 0; i < k; i++) {
+//         a[i] = arr[i];
+//     }
+//     for (i = (k - 1 - 1) / 2; i >= 0; i--) {
+//         AdjustDown(a, k, i);
+//     }
+//     //遍历剩下的数据,若小于堆顶则替换堆顶，并进行向下调整
+//     for (i = k; i < arrSize; i++) {
+//         if (arr[i] < a[0]) {
+//             a[0] = arr[i];
+//             AdjustDown(a, k, 0);
+//         }
+//     }
+//     *returnSize = k;
+//     return a;
+// }
+
+
+
+// ----------------------------------链式二叉树-----------------------------
+
+        //     A  
+        //    / \  
+        //   B   C
+        //  / \    
+        // D   E 
+// 遍历二叉树有4种遍历：前序，中序，后序，层序
+// 前序，中序，后序为深度优先   层序为广度优先
+
+// 将任何一颗一颗二叉树看成3个部分(分析的时候要以树为一个单位)
+// 1.根节点     2.左子树    3.右子树
+
+// 前序：(先根遍历) 根 左子树 右子树
+// A B D NULL NULL E NULL NULL C NULL NULL 
+// 中序：(中根遍历) 左子树 根 右子树
+// NULL D NULL B NULL E NULL A NULL C NULL
+// 后序：(后根) 左子树 右子树 根
+// NULL NULL D NULL NULL E B NULL NULL C A
+// 层序：
+// A B C D E NULL NULL NULL NULL NULL NULL
+
+// int TreeDepth(BTNode* root) {
+//     if (root == NULL)
+//         return 0;
+
+//     int leftDepth = TreeDepth(root->left);
+//     int rightDepth = TreeDepth(root->right);
+
+//     return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
+// }
+
+
+// 二叉树的思路还是递归
+// 1.将大问题化为子问题
+// 2.返回条件（子问题不能继续缩小了）
+
+// 144. 二叉树的前序遍历
+// https://leetcode.cn/problems/binary-tree-preorder-traversal/
+
+//先求出树的大小并开辟空间，然后再写一个函数前序遍历，可以用returnSize控制数组的下标,
+//或者用新的i传入
+
+// int TreeSize(struct TreeNode* root) {
+//     if (root == NULL) 
+//         return 0;
+//     else
+//         return 1 + TreeSize(root->left) + TreeSize(root->right);
+// }
+
+// void PrevOrder(struct TreeNode* root, int* returnSize, int* a) {
+//     if (root == NULL) return;
+//     a[(*returnSize)++] = root->val;
+//     PrevOrder(root->left, returnSize, a);
+//     PrevOrder(root->right, returnSize, a);
+// }
+
+// int* preorderTraversal(struct TreeNode* root, int* returnSize) {
+//     int size = TreeSize(root);
+//     int* a = malloc(sizeof(int) * size);
+//     //returnSize初始值手动赋值为0
+//     *returnSize = 0;
+//     PrevOrder(root, returnSize, a);
+//     return a;
+// }
+
 
